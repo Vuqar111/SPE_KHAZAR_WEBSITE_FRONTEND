0404eba19bb42827eee7fca3c6c6548bd58e34ec
asimmakhmudov <asimmahmudov18@gmail.com>
2022-10-20 21:55:54 +0400

    update

--- src/data.js
+++ src/data.js
@@ -17,6 +17,7 @@ import ruslan from "./assets/images/teamImages/ruslan.jpg";
 import aqsin from "./assets/images/teamImages/aqsin.png";
 import rashad from "./assets/images/teamImages/resad.jpg";
 import zeyneb from "./assets/images/teamImages/zeyneb.jpeg";
+import aysu from "./assets/images/teamImages/aysu.jpg";
 export const teams = [
   {
     id: 1,
@@ -117,18 +118,24 @@ export const teams = [
   },
   {
     id: 17,
+    name: "Aysu Mammadli",
+    status: "Designer",
+    img: aysu,
+  },
+  {
+    id: 18,
     name: "Aqsin Qarashli",
     status: "Former Vice President",
     img: aqsin,
   },
   {
-    id: 18,
+    id: 19,
     name: "Rashad Musayev",
     status: "Former Program Chairperson",
     img: rashad,
   },
   {
-    id: 19,
+    id: 20,
     name: "Zeynab Farzizada",
     status: "Former Social Activities Chair",
     img: zeyneb,
