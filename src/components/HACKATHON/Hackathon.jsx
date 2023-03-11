813a64f866af884a1fb385de366cf25ed092f595
Vuqar111 <vuqar.hesenov_2003@mail.ru>
2023-03-11 12:54:25 +0400

    mib

--- src/components/HACKATHON/Hackathon.jsx
+++ src/components/HACKATHON/Hackathon.jsx
@@ -4,13 +4,15 @@ import khazarLogo from "../../assets/images/hackathon/khazar.png";
 import speLogo from "../../assets/images/hackathon/spekhazarlogo.png";
 import mlkLogo from "../../assets/images/hackathon/mlklogo.png";
 import codersLogo from "../../assets/images/hackathon/coders.svg";
-import AOS from "aos";
-import "aos/dist/aos.css";
+// import AOS from "aos";
+// import "aos/dist/aos.css";
 
 const Hackathon = () => {
-  useEffect(() => {
-    AOS.init({ duration: 2000 });
-  }, []);
+  // useEffect(() => {
+  //   AOS.init({ duration: 2000 });
+  // }, []);
+
+
   return (
     <>
       <div className="hackContainer">
@@ -24,7 +26,7 @@ const Hackathon = () => {
 
         <h2 className="hackathonsubheader">About</h2>
         <div className="hackbox">
-          <div className="hackcontent" data-aos="fade-right">
+          <div className="hackcontent" >
             <p className="hacktext">
               UFAZ Hackathon is a thematic event during which competitors from
               various universities work intensively on different projects with
@@ -59,7 +61,7 @@ const Hackathon = () => {
             className="hackcontentimg"
             src="https://res.cloudinary.com/dmpt1iii5/image/upload/v1665521469/SPE-Events/284470749_564616025105668_5927146446129160428_n_1_edx9by.jpg"
             alt=""
-            data-aos="fade-left"
+        
           />
         </div>
 
@@ -70,9 +72,9 @@ const Hackathon = () => {
             className="hackcontentimg"
             src="https://res.cloudinary.com/dmpt1iii5/image/upload/v1665521469/SPE-Events/284470749_564616025105668_5927146446129160428_n_1_edx9by.jpg"
             alt=""
-            data-aos="fade-right"
+      
           />
-          <div className="hackcontent" data-aos="fade-left">
+          <div className="hackcontent">
             <ul className="hackList">
               <li className="hacktext">
                 Only students can compete at the Hackathon
